89350f99add57e467aaa0b314c2af52fc7b0cd4b
Gerard <roza@univ-orleans.fr>
2020-03-09 10:27:31 +0100

    added TopAppBar and heading to  example

--- App.svelte
+++ App.svelte
@@ -1,3 +1,21 @@
+<svelte:head>
+  <title>SMUI example</title>
+  <link href="https://fonts.googleapis.com/css?family=Material+Icons&display=block" rel="stylesheet">
+</svelte:head>
+<TopAppBar variant="static" {prominent} {dense} color={secondaryColor ? 'secondary' : 'primary'}>
+        <Row>
+          <Section>
+            <IconButton class="material-icons">menu</IconButton>
+            <Title>Static</Title>
+          </Section>
+          <Section align="end" toolbar>
+            <IconButton class="material-icons" aria-label="Download">file_download</IconButton>
+            <IconButton class="material-icons" aria-label="Print this page">print</IconButton>
+            <IconButton class="material-icons" aria-label="Bookmark this page">bookmark</IconButton>
+          </Section>
+        </Row>
+      </TopAppBar>
+
 <div class="container">
   <Button on:click={() => clicked++}>
     <Icon class="material-icons">thumb_up</Icon>
@@ -13,8 +31,14 @@
 </div>
 
 <script>
+  import TopAppBar, {Row, Section, Title} from '@smui/top-app-bar';
+  
+  let prominent = false;
+  let dense = false;
+  let secondaryColor = false;
   import Button, {Label, Icon} from '@smui/button';
-
+  import IconButton from '@smui/icon-button';
+  
   let clicked = 0;
 </script>
 
